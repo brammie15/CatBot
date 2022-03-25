e85f2d77d24f64035d245c477c668edbf146d30c
brammie15 <Brammieverhulst@gmail.com>
2022-03-25 18:51:07 +0100

    Initial commit

---
+++ src/main/java/com/brammie15/bot/Bot.java
@@ -0,0 +1,17 @@
+package com.brammie15.bot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar bot.jar <token>");
+            System.exit(1);
+        }
+        JDA api = JDABuilder.createDefault(args[0]).build();
+        api.addEventListener(new CatListener());
+    }
+}
+
+
